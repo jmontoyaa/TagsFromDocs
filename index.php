907d101208cbc413f9f191f5a62118460e2270b8
Julio Montoya <gugli100@gmail.com>
2014-06-05 18:44:17 +0200

    Adding weird requirement. (load date and checkboxes)

--- index.php
+++ index.php
@@ -17,9 +17,29 @@ if (file_exists($fileName)) {
 
     $word = \PhpOffice\PhpWord\IOFactory::load($fileName);
     $tags = $word->getTags();
+    echo '<pre>';
     foreach ($tags as $tag) {
-        if (in_array($tag['property']['tag'], $tagsWanted)) {
-            echo $tag['property']['alias'] . ': ' . $tag['content']['content'] . PHP_EOL;
+        $isTag = isset($tag['property']['tag']) && in_array($tag['property']['tag'], $tagsWanted);
+        if (isset($tag['property']) && isset($tag['property']['alias']) &&
+            (
+                $isTag ||
+                isset($tag['property']['is_checkbox']) && $tag['property']['is_checkbox'] ||
+                isset($tag['property']['is_date']) && $tag['property']['is_date']
+            )
+        ) {
+            echo $tag['property']['alias'] . ': ' .
+                $tag['content']['content'];
+
+            if ($isTag) {
+
+                /*if (is_float($tag['content']['content'])) {
+                    echo ' Is float: true';
+                } else {
+                    echo ' Is float: false';
+                }*/
+            }
+
+            echo PHP_EOL;
         }
     }
 
@@ -27,6 +47,6 @@ if (file_exists($fileName)) {
     $template = new Template($fileName);
     file_put_contents(basename($fileName) . '.xml', $template->documentXML);
 } else {
-    echo "File doens't exists $fileName";
+    echo "File doesn't exists $fileName";
 }
 exit;
